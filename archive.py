2410a26d13354fe60b464de36f151cd62875c456
mcdr22101 <mcdr22101@wainuiomatahigh.school.nz>
2025-05-08 12:44:53 +1200

    Update archive.py

--- archive.py
+++ archive.py
@@ -1,1 +1,6 @@
-
+planeseats={"flight1":10,"flight2":10,"flight3":10}
+name=input("What is your name? ")
+desiredflight=int(input("Which flight would you like to take? (1 for flight 1, 2 for flight 2, 3 for flight 3) "))
+desiredflight=desiredflight-1
+if desiredflight==0:
+    print(planeseats{"flight1"})
