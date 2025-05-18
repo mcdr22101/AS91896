42698f7c81af491879447c5a5f115720e2bdb3df
mcdr22101 <mcdr22101@wainuiomatahigh.school.nz>
2025-05-18 22:37:05 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 """
-setting up variables
+setting up variables and randomising next day
 """
+import random
+nextday=random.randint(1,7)
+planeseats={"flight 1":10, "flight 2":5,"flight 3":0}
+planeprice={"flight 1":100,"flight 2":150,"flight 3":200}
+if nextday==7:
+    planeprice={"flight 1":100*0.8,"flight 2":150,"flight 3":200}
+print(nextday)
 LIST_VALIDFLIGHTS = ["flight 1", "flight 2", "flight 3"]
-
-"""name asking function"""
-str_name=("")
-def func_nameask():
-    name=input("What is your name? ")
-    return(name)
-func_nameask()
+name=("qqqqqqqqqqqqqqqqqqqqq")
+"""asking the user for their name and making sure the name is not too long"""
+while len(name) > 20:
+ name=input("What is your name? ")
 print("Hello",(name)+". Please select a flight.")
-dict_planeseats={"flight 1":10,"flight 2":20,"flight 3":20}
-dict_planeprice={"flight 1":100,"flight 2":150,"flight 3":200}
 x=0
+"""asking user which flight they will take"""
 while x==0:
-    desiredflight=(input("Which flight would you like to take? (type flight 1, flight 2, or flight 3) "))
+    desiredflight=(input("Which flight would you like to take? " 
+    "(type flight 1, flight 2, or flight 3) "))
     if desiredflight in LIST_VALIDFLIGHTS:
         x=1
-        
-
+desiredflightprice=(planeprice[(desiredflight)])
+"""function for generating discount"""
+def func_discount():
+    discounteddesiredflightprice=(planeprice[(desiredflight)])*0.9
+    return(discounteddesiredflightprice)
 print(planeseats[(desiredflight)])
 if (planeseats[(desiredflight)])>9:
-    desiredflightprice=(planeprice[(desiredflight)])
-    discounteddesiredflightprice=(planeprice[(desiredflight)])*0.8
-    print("There is a discount available if you fly with us tomorrow. The flight will cost",(discounteddesiredflightprice),"instead of",(desiredflightprice))
+    func_discount()
+"""telling user whether there is a discount available or not"""
+    print("There is a discount available if you fly with us tomorrow."
+    "The flight will cost",(func_discount()),"NZD instead of",(desiredflightprice),"NZD.")
+    if nextday==7:
+        print("There is also an additional next day discount available because the next day is sunday.")
 else:
     desiredflightprice=(planeprice[(desiredflight)])*1
-    print("There is no discount available.")
-print(desiredflightprice)
+    print("There is no discount available. Your flight will cost",(planeprice[(desiredflight)]),"NZD.")
